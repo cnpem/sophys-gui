20d87fb85d7e90893f867d036156c7359c807a1a
rlyra8 <rafael.lyra@lnls.br>
2024-01-31 15:08:29 -0300

    Improve server callback

--- sophys_gui/server/model.py
+++ sophys_gui/server/model.py
@@ -2,13 +2,17 @@ import os
 import sys
 import time
 
+from functools import partial
 from bluesky_queueserver_api import WaitMonitor
 from bluesky_widgets.models.run_engine_client import RunEngineClient
+
+from qtpy.QtWidgets import QApplication
 
 from suitscase.utilities.threading import AsyncFunction
 
 
 class QueueServerModel:
+
     def __init__(self):
         http_server_api_key = os.environ.get("QSERVER_HTTP_SERVER_API_KEY", None)
 
@@ -17,7 +21,12 @@ class QueueServerModel:
             http_server_api_key=http_server_api_key,
         )
 
-        self.monitor_queue_changes()
+        self.server_callback(
+            lambda status: status["plan_queue_uid"] != self.run_engine._plan_queue_uid,
+            self.run_engine.load_plan_queue)
+        self.server_callback(
+            lambda status: status["plan_queue_mode"] != self.run_engine.events.status_changed,
+            partial(self.run_engine.load_re_manager_status, unbuffered=True))
 
     def exit(self):
         self.__queue_monitor.cancel()
@@ -25,16 +34,19 @@ class QueueServerModel:
             time.sleep(0.01)
 
     @AsyncFunction
-    def monitor_queue_changes(self):
-        self.__monitoring_queue = True
-        self.__queue_monitor = WaitMonitor()
-        while True:
-            self.__queue_uid = self.run_engine._plan_queue_uid
-            try:
-                self.run_engine._client.wait_for_condition(lambda status: status["plan_queue_uid"] != self.__queue_uid, timeout=sys.float_info.max, monitor=self.__queue_monitor)
-            except self.run_engine._client.WaitCancelError:
-                break
-            except self.run_engine._client.WaitTimeoutError:
-                pass
-            self.run_engine.load_plan_queue()
-        self.__monitoring_queue = False
+    def server_callback(self, param, cmd):
+        def monitor_queue_changes():
+            self.__monitoring_queue = True
+            self.__queue_monitor = WaitMonitor()
+            while True:
+                try:
+                    print(param)
+                    self.run_engine._client.wait_for_condition(
+                        param, timeout=sys.float_info.max, monitor=self.__queue_monitor)
+                except self.run_engine._client.WaitCancelError:
+                    break
+                except self.run_engine._client.WaitTimeoutError:
+                    pass
+                cmd()
+            self.__monitoring_queue = False
+        monitor_queue_changes()
