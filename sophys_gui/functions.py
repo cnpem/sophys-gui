22c38bfd5e5bec583bb1d0821616fad3f5a7a2cb
rlyra8 <rafael.lyra@lnls.br>
2024-02-08 09:57:51 -0300

    Fix instruction missing fields error

--- sophys_gui/functions.py
+++ sophys_gui/functions.py
@@ -6,7 +6,10 @@ from qtpy.QtWidgets import QLabel
 def getItemRecursively(original_obj: object, attrs: list):
     _prev = original_obj
     for attr in attrs:
-        _prev = getattr(_prev, attr, _prev[attr])
+        if attr in _prev:
+            _prev = getattr(_prev, attr, _prev[attr])
+        else:
+            return ''
     return _prev
 
 
