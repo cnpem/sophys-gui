67719753a2e1c4cf9c50577575c2a5888301f1a6
rlyra8 <rafael.lyra@lnls.br>
2024-02-27 14:47:36 -0300

    Refactor Queue Monitor

--- sophys_gui/components/__init__.py
+++ sophys_gui/components/__init__.py
@@ -1,7 +1,7 @@
 from .table_view import SophysQueueTable, SophysHistoryTable
 from .switch import SophysSwitchButton
 from .running_item import SophysRunningItem
-from .queue_controller import QueueController
+from .queue_controller.main import QueueController
 from .application import SophysApplication
 from .input import SophysInputList, SophysInputDict, SophysSpinBox, \
     SophysInputMotor
