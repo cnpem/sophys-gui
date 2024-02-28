15bf434b22f8a049cd488446d51e2f83bd9100e5
rlyra8 <rafael.lyra@lnls.br>
2024-02-28 15:58:24 -0300

    Refactor running item widget

--- sophys_gui/components/running_item/main.py
+++ sophys_gui/components/running_item/main.py
@@ -10,6 +10,18 @@ from suitscase.utilities.threading import DeferredFunction
 
 
 class SophysRunningItem(QWidget):
+    """
+        Widget that displays the parameters of the current running item.
+
+        .. note::
+            A loading icon will appear if the process is running.
+
+        .. image:: ./_static/running.png
+            :width: 300
+            :alt: Running Item Widget
+            :align: center
+
+    """
 
     def __init__(self, model):
         super().__init__()
@@ -26,72 +38,101 @@ class SophysRunningItem(QWidget):
             btn = createSingleBtn(btnValue, self.runEngine)
             glay.addWidget(btn, 6, idy, 1, 2)
 
-    def runningItemChanged(self, evt):
-        runningItem = self.runEngine._running_item
-        self.getItemAttributes(runningItem)
-
     def createDictionaryWidget(self, arg_dict):
+        """
+            Create a widget that displays a dictionary.
+        """
         widget = QWidget()
         lay = QGridLayout()
         widget.setLayout(lay)
         idx = 0
-        isDict = isinstance(arg_dict, dict)
-        key_list = arg_dict
-        if isDict:
-            key_list = list(arg_dict.keys())
+        key_list = list(arg_dict.keys())
         for key in key_list:
             title = QLabel(key)
             title.setStyleSheet("font-weight: 300;")
             lay.addWidget(title, idx, 0, 1, 1)
 
-            if isDict:
-                value = QLabel(str(arg_dict[key]))
-                value.setWordWrap(True)
-                value.setAlignment(Qt.AlignCenter)
-                lay.addWidget(value, idx, 1, 1, 1)
+            value = QLabel(str(arg_dict[key]))
+            value.setWordWrap(True)
+            value.setAlignment(Qt.AlignCenter)
+            lay.addWidget(value, idx, 1, 1, 1)
             idx += 1
 
         return widget
 
-    @DeferredFunction
-    def getItemAttributes(self, runningItem):
-        args_list = ['kwargs', 'user', 'name']
-        group = QGroupBox()
-        glay = QGridLayout()
-        group.setLayout(glay)
-        idx = 0
-        idy = 0
-        for key, item in runningItem.items():
-            if key in args_list:
-                item_group = QGroupBox()
-                lay = QHBoxLayout()
-                item_group.setLayout(lay)
-                item_group.setTitle(key)
-                stretch = 2 if key == "kwargs" else 1
-                glay.addWidget(item_group, idx, idy, 1, stretch)
-
-                if isinstance(item, str):
-                    value = QLabel(item)
-                    value.setAlignment(Qt.AlignCenter)
-                else:
-                    argsList = [
-                        runningItem["args"], runningItem["kwargs"]
-                    ]
-                    if len(argsList[0]) > 0:
-                        addArgsToKwargs(argsList)
-                    value = self.createDictionaryWidget(argsList[1])
-                lay.addWidget(value)
-
-                idy += 1
-                if idy >= 2:
-                    idy = 0
-                    idx += 2
-
+    def updateRunningItem(self, group):
+        """
+            Update running item widget.
+        """
         self.group.deleteLater()
         self.attributesDisplay.addWidget(group)
         self.group = group
 
+    def handleArgsList(self, runningItem):
+        """
+            Append the args into the kwargs dictionary.
+        """
+        if not "kwargs" in runningItem:
+            runningItem["kwargs"] = {}
+        if "args" in runningItem:
+            hasArgs = len(runningItem["args"]) != 0
+            if hasArgs:
+                argsList = [
+                    runningItem["args"], runningItem["kwargs"]
+                ]
+                addArgsToKwargs(argsList)
+        return runningItem["kwargs"]
+
+    def getValueWidget(self, runningItem, key):
+        """
+            Create the widget for showing the key value.
+        """
+        item = {}
+        if key in runningItem:
+            item = runningItem[key]
+        if isinstance(item, str):
+            value = QLabel(item)
+            value.setAlignment(Qt.AlignCenter)
+        else:
+            argsDict = self.handleArgsList(runningItem)
+            value = self.createDictionaryWidget(argsDict)
+        return value
+
+    @DeferredFunction
+    def updateRunningItemWidget(self, evt):
+        """
+            Update the widget showing the current running item.
+        """
+        args_list = ["user", "name", "kwargs"]
+        group = QGroupBox()
+        glay = QGridLayout()
+        group.setLayout(glay)
+        runningItem = self.runEngine._running_item
+        pos = [0, 0]
+        if len(runningItem) == 0:
+            return
+
+        for key in args_list:
+            item_group = QGroupBox()
+            lay = QHBoxLayout()
+            item_group.setLayout(lay)
+            item_group.setTitle(key)
+            stretch = 2 if key == "kwargs" else 1
+            glay.addWidget(item_group, *pos, 1, stretch)
+
+            valueWid = self.getValueWidget(runningItem, key)
+            lay.addWidget(valueWid)
+
+            pos[1] += 1
+            if pos[1] >= 2:
+                pos[1] = 0
+                pos[0] += 2
+        self.updateRunningItem(group)
+
     def createLoading(self):
+        """
+            Create loading icon to indicate if the queue is running.
+        """
         statusComp = lambda stateVar: stateVar == "executing_queue"
         statusKey = "manager_state"
         return SophysLed(
@@ -113,4 +154,4 @@ class SophysRunningItem(QWidget):
         self.createBtns(glay)
 
         self.runEngine.events.running_item_changed.connect(
-            self.runningItemChanged)
+            self.updateRunningItemWidget)
