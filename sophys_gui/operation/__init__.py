c3d0da3c67dff487adf8894096400725d7ca2c52
rlyra8 <rafael.lyra@lnls.br>
2024-03-19 09:23:13 -0300

    Document operation GUI

--- sophys_gui/operation/__init__.py
+++ sophys_gui/operation/__init__.py
@@ -1,1 +1,27 @@
+"""
+    The Sophys GUI is designed for controlling and monitoring the results of a Bluesky
+    scan using a Queue Server.
+
+    .. image:: ./_static/sophys_gui.png
+        :width: 500
+        :alt: Sophys GUI
+        :align: center
+
+
+    How to access it
+    ================
+
+    You can access this GUI with a command in the terminal.
+
+    .. code-block:: python
+        :linenos:
+
+        sophys-gui
+
+    .. attention::
+        You need to login in order to control the Run Engine or the Queue.
+        If you are not logged in, you will only be able to monitor them.
+
+"""
+
 from .main import SophysOperationGUI
