35e5d117d2668f9f9248c0c4c210b33d0aea4115
Rafael Lyra <rafael.lyra@lnls.br>
2024-05-27 15:00:42 -0300

    Fix update enviroment

--- sophys_gui/components/running_item/util.py
+++ sophys_gui/components/running_item/util.py
@@ -2,6 +2,6 @@ CONTROL_BTNS = [
     {
         "title": "Update Environment",
         "icon": "mdi6.update",
-        "cmd": lambda re: re.environment_update
+        "cmd": lambda re: re.environment_update()
     }
 ]
