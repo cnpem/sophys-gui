d4377c1655029aae21d4600591bffdce3795b40e
Rafael Lyra <rafael.lyra@lnls.br>
2024-07-30 14:02:44 -0300

    Add support for api key authentication

--- sophys_gui/__main__.py
+++ sophys_gui/__main__.py
@@ -13,11 +13,12 @@ from sophys_gui.operation import SophysOperationGUI
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--http-server", required=True, help="The httpserver address to connect to. (e.g. http://127.0.0.1:http_server_port)")
+    parser.add_argument("--http-server-api-key", required=False, help="The httpserver authentication api key")
     parser.add_argument("--kafka-bootstrap", required=True, help="The Kafka broker address to connect to. (e.g. 127.0.0.1:kafka_port)")
     parser.add_argument("--kafka-topic", required=True, help="The Kafka topic to listen to. (e.g. test_bluesky_raw_docs)")
     args = parser.parse_args()
 
-    __backend_model = ServerModel(args.http_server)
+    __backend_model = ServerModel(args.http_server, args.http_server_api_key)
 
     app = SophysApplication(sys.argv)
 
