82e33738eda09ee65213334afc6a043a45a17d5a
Rafael Lyra <rafael.lyra@lnls.br>
2025-02-25 11:08:28 -0300

    Export some Sophys components for use in other GUIs

--- sophys_gui/__init__.py
+++ sophys_gui/__init__.py
@@ -1,3 +1,6 @@
 """
     This is a GUI for controlling and monitoring a Blueksy instance through HTTP Server and Kafka.
 """
+from .server.model import ServerModel
+from .components.form import SophysForm
+from .components.login import SophysLogin
