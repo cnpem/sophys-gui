eceeefcc2c2f9e3ce9043a507db392b995d1af92
rafael.lyra <rafael.lyra@lnls.br>
2025-08-11 13:31:22 -0300

    Update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sophys-gui',
-    version='0.11.0',
+    version='0.11.1',
     author='SWC - LNLS',
     description='Control GUI for the Bluesky queue.',
     install_requires=[
