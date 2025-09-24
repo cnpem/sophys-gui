3031d52ee461d997fb65930e00eb794aceb1b7b2
rlyra8 <63302930+RafaelLyra8@users.noreply.github.com>
2025-09-24 08:46:28 -0300

    Update version to 0.14.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sophys-gui',
-    version='0.14.2',
+    version='0.14.3',
     author='SWC - LNLS',
     description='Control GUI for the Bluesky queue.',
     install_requires=[
